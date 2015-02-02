52f54c0ae2398e626bd77cabc48f35fc92d191b3
bazuccka <sarazucc@gmail.com>
2015-02-02 13:24:42 -0600

    bottstrap map

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php include 'database.php'; ?>
-<?PHP 
+<?php
 	//Get Total Nmber of Questions
 	$query ="SELECT * FROM questions";
 	//GET RESULTS
@@ -9,7 +9,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8" />
+		<meta http-equiv="Content-type" value="text/html; charset=UTF-8" />
 		<title>Quizlet</title>
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 		<link href="css/bootstrap.css" rel="stylesheet">
