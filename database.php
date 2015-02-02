deba016c9109019e53c97dd2236acdb96db1ec28
Sara Zuccarello <sarazucc@gmail.com>
2015-02-02 13:38:42 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php 
 //Create connection credentials
-$db_host = 'localhost';
+$db_host = 'quizlet.cqk9ew5iyc62.us-west-2.rds.amazonaws.com';
 $db_name = 'quizlet';
-$db_user = 'root';
-$db_pass = 'insignia89';
+$db_user = 'bazuccka';
+$db_pass = '*fcGU6wVPpx7';
 
 //Create mysqli object
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
