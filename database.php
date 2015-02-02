f2ef1c9d6115d200f566c09788631790d150b598
Sara Zuccarello <sarazucc@gmail.com>
2015-02-02 13:37:51 -0600

    cleaned up

--- database.php
+++ database.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 //Create connection credentials
 $db_host = 'localhost';
@@ -14,59 +13,3 @@ if($mysqli->connect_error) {
 	printf("Connect failed: %s\n", $mysqli->connect_error);
 	exit();
 }
-
-?>
-
-
-<!--aws
-	DB instance Id: quizlet
-	DB name: quizlet
-	username: bazuccka
-	password: qc^M6Ben^#90
--->
-=======
-<?php 
-//Create connection credentials
-$db_host = 'localhost';
-$db_name = 'quizlet';
-$db_user = 'root';
-$db_pass = 'insignia89';
-
-//Create mysqli object
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-//Error handler
-if($mysqli->connect_error) {
-	printf("Connect failed: %s\n", $mysqli->connect_error);
-	exit();
-}
-
-?>
-
-
-<!--aws
-	DB instance Id: quizlet
-	DB name: quizlet
-	username: bazuccka
-	password: qc^M6Ben^#90
--->
->>>>>>> origin/master
-=======
-<?php 
-//Create connection credentials
-$db_host = 'quizlet.cqk9ew5iyc62.us-west-2.rds.amazonaws.com';
-$db_name = 'quizlet';
-$db_user = 'bazuccka';
-$db_pass = '*fcGU6wVPpx7';
-
-//Create mysqli object
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-//Error handler
-if($mysqli->connect_error) {
-	printf("Connect failed: %s\n", $mysqli->connect_error);
-	exit();
-}
-
-?>
->>>>>>> c6a0076a952e324985cd502768a3e874065078d0
