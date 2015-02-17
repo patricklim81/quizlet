28a42d1f6b23d594efb57fddce5d34edd075ea0f
bazuccka <sarazucc@gmail.com>
2015-02-17 09:19:55 -0600

    fixed footer issue of not sticking

--- question.php
+++ question.php
@@ -46,12 +46,6 @@
 				</ul>
 			</form>
 		</div>
-<div class="progress">
-  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
-  <!--  var session = ?php echo $_SESSION['session']; ?>;-->
-  </div>
-</div>
-
 </div>
 	</main>
 <?php include 'includes/footer.php'; ?>
